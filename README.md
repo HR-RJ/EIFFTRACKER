1b9fd436eb6a1bd4b5597969070971b6fbce89ac
RJ <1035471@hr.nl>
2023-07-17 16:51:59 +0200

    works?

---
+++ README.md
@@ -0,0 +1,6 @@
+# EIFFTRACKER(TFT discord tracker)
+this bot tracks the TFT stats of registered users in a discord
+
+# Installation
+
+# Example
--- main.py
+++ main.py
@@ -1,28 +1,63 @@
 import os
 from dotenv import load_dotenv
+import discord
 from discord.ext import commands
-from riotwatcher import LolWatcher, ApiError
+from riotwatcher import LolWatcher, TftWatcher, ApiError
+import json
+import random
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 RIOT_API_KEY = os.getenv('RIOT_API_KEY')
 REGION = os.getenv('RIOT_REGION')  # Add this line to read the region from environment variables
 
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+data_file = "data.json"
+tft_watcher = TftWatcher(RIOT_API_KEY)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-@bot.command(name='tft', help='Responds with user\'s TFT games')
-async def tft(ctx, summoner_name: str):
-    lol_watcher = LolWatcher(RIOT_API_KEY)
-    my_region = REGION  # Change this line to use the region from environment variables
-    
+def search(id, list):
+    return [element['id'] for element in list if element['disc_id'] == id]
+
+@bot.command(name='register', help="Register your summoner to Pengu")
+async def register(ctx, summoner_name: str):
+    data = json.loads(data_file)
     try:
-        summoner = lol_watcher.summoner.by_name(my_region, summoner_name)
-        games = lol_watcher.tft_match.by_puuid(my_region, summoner['puuid'])
+        summoner = tft_watcher.summoner.by_name(REGION, summoner_name)
+        await ctx.send(f'Summoner {summoner_name} has been registered.')
+    except ApiError as err:
+        if err.response.status_code == 429:
+            await ctx.send('We have hit the rate limit. Please try again later.')
+        else:
+            await ctx.send('There was an error getting the data.')
+
+
+@bot.command(name='tft', help='Lookup a tft user with rank and last 5 games')
+async def tft(ctx, summoner_name: str):
+    data = json.loads(data_file)
+    try:
+        summoner_id = data[ctx.author.id]['id']
+        summoner = tft_watcher.league.by_summoner(REGION, summoner)
+        games = tft.tft_match.by_puuid(REGION, summoner['puuid'])
         await ctx.send(f'Summoner {summoner_name} has played {len(games)} TFT games.')
     except ApiError as err:
         if err.response.status_code == 429:
             await ctx.send('We have hit the rate limit. Please try again later.')
         else:
             await ctx.send('There was an error getting the data.')
+
+@bot.command(name='firstoreiff', help='Responds with "First or Eiff"')
+async def firstoreiff(ctx):
+    
+    rand = random.randint(0,1)
+    if rand == 0:
+        await ctx.send('Issa :trophy: First :trophy:')
+    else:
+        await ctx.send('It\'s an :skull: Eiff :skull:')
+
+    
+@client.event 
+async def on_ready(): print("online")
 
 bot.run(TOKEN)
